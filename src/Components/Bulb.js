ca445c46e6d30771e83e6e0dda19c83107603089
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-09 21:31:55 +0700

    bulbswitch tren lop

---
+++ src/Components/Bulb.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+const on_img = './on.jpg'
+const off_img = './off.jpg'
+// function switch_bulb(){
+//     console.log("you click switch button")
+
+// }
+export class Bulb extends React.Component{
+    constructor (props){
+        super(props)
+        this.state={  // this là toàn bộ class và this là {}
+            isOn:true
+        }
+        this.switch_bulb = this.switch_bulb.bind(this)
+    }
+    switch_bulb(){
+        // console.log("you click switch button")
+        // thay doi state
+        this.setState({isOn:!this.state.isOn})
+    }
+    render(){
+        return (
+            // <div >{100>10?"true":"false"}</div>    // trong {} la 1 bieu thuc js ra ket qua nen if else k dc
+            <div>
+                <img src={this.state.isOn ? on_img : off_img}/>
+                <input type ="button" value="Switch" onClick={this.switch_bulb} />
+            </div>
+        )
+    }
+    
+}
