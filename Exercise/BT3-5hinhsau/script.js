94ea010902e1d1c0b458ab5754e78fee7b52c2bd
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-10 13:22:22 +0700

    xong bai 6 , 10

--- Exercise/BT3-5hinhsau/script.js
+++ Exercise/BT3-5hinhsau/script.js
@@ -46,20 +46,22 @@
 //     [4, 9, 2]
 //   ]))
 
-// 9. The Fiscal Code
-// Each person in Italy has an unique identifying ID code issued by the national tax office after the birth registration: the Fiscal Code (Codice Fiscale). Check the Resources tab for more info on this.
-// Given an object containing the personal data of a person (name, surname, gender and date of birth) return the 11 code characters as a string following these steps:
-// Generate 3 capital letters from the surname, if it has:
-// At least 3 consonants then the first three consonants are used.(Newman -> NWM).
-// Less than 3 consonants then vowels will replace missing characters in the same order they appear (Fox -> FXO | Hope -> HPO).
-// Less than three letters then "X" will take the third slot after the consonant and the vowel (Yu -> YUX).
-// Generate 3 capital letters from the name, if it has:
-// Exactly 3 consonants then consonants are used in the order they appear (Matt -> MTT).
-// More than 3 consonants then first, third and fourth consonant are used (Samantha -> SNT | Thomas -> TMS).
-// Less than 3 consonants then vowels will replace missing characters in the same order they appear (Bob -> BBO | Paula -> PLA).
-// Less than three letters then "X" will take the the third slot after the consonant and the vowel (Al -> LAX).
-// Generate 2 numbers, 1 letter and 2 numbers from date of birth and gender:
-// Take the last two digits of the year of birth (1985 -> 85).
-// Generate a letter corresponding to the month of birth (January -> A | December -> T) using the table for conversion included in the code.
-// For males take the day of birth adding one zero at the start if is less than 10 (any 9th day -> 09 | any 20th day -> 20).
-// For females take the day of birth and sum 40 to it (any 9th day -> 49 | any 20th day -> 60).
+// 10. Number of Boomerangs
+// A boomerang is a V-shaped sequence that is either upright or upside down. Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+// Some boomerang examples:
+// [3, 7, 3], [1, -1, 1], [5, 6, 5]
+// Create a function that returns the total number of boomerangs in an array.
+
+// function countBoomerangs(array) {
+//     let count = 0
+//     for (let i = 0; i < array.length - 2; i++) {
+//         if (array[i] == array[i + 2] && array[i] != array[i + 1]) {
+//             count += 1
+//         }
+//     }
+//     return count
+// }
+
+// console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]))
+// console.log(countBoomerangs([5, 6, 6, 7, 6, 3, 9]))
+// console.log(countBoomerangs([4, 4, 4, 9, 9, 9, 9]))
