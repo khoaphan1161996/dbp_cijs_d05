e4739a9ddd75263c2dff9ebe081ce4371b704a8e
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-14 01:07:26 +0700

    bai 7

--- Exercise/BT4-67810/script.js
+++ Exercise/BT4-67810/script.js
@@ -86,6 +86,42 @@
 // digitaldrome(666)
 // digitaldrome(1985)
 
+// 7. Poker Flush?
+// Create a function that takes in two arrays and determines whether there exists a flush.
+// The first array represents the 5 cards dealt on the table. The second array represents the 2 cards in your hand. Notation: card number and suit (abbreviated as S = Spades, H = Hearts, D = Diamonds, C = Clubs) separated by an underscore.
+
+// function checkFlush(array1,array2){
+//     let res = []
+//     let check = true
+
+//     for(let i = 0 ; i < array2.length;i++){
+//         let chieudai = array2[i].length
+//         res.push(array2[i][chieudai - 1])
+//     }
+//     array1 = array1.concat(res)
+
+//     for(let j = 0 ; j  < res.length ; j++){
+//         let count = 0
+//         let a = res[j]
+//         for(let i = 0 ; i < array1.length; i++){
+//             let chieudai = array1[i].length
+//             if(a==array1[i][chieudai - 1]){
+//                 count += 1
+//             }
+//         }
+//         // console.log(count)
+//         if(count < 5){
+//             check = false
+//         }
+//     }
+    
+//     console.log(check)
+// }
+
+// checkFlush(["A_S", "J_H", "7_D", "8_D", "10_D"], ["J_D", "3_D"])
+// checkFlush(["10_S", "7_S", "9_H", "4_S", "3_S"], ["K_S", "Q_S"])
+// checkFlush(["3_S", "10_H", "10_D", "10_C", "10_S"], ["3_S", "4_D"])
+
 // 8. Dice Gambling
 // Create a function that takes an array consisting of dice rolls from 1-6. Return the sum of your rolls with the following conditions:
 // If a 1 is rolled, that is bad luck. The next roll counts as 0. If a 6 is rolled, that is good luck. The next roll is multiplied by 2. The array length will always be 3 or higher.
