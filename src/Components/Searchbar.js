2673bea7c41310523cdb5775382481d2b6d58c3a
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-14 22:09:26 +0700

    11/14

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,5 +1,18 @@
-import React from 'react'
 import '../assets/css/searchbar.css'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import {firebaseConfig} from '../config/firebase.config'
+import React from 'react'
+// di theo nhom
+try{
+    firebase.initializeApp(firebaseConfig);
+}catch(e){
+    console.log(e.message)
+}
+
+const db = firebase.firestore();
+
 export class SearchBar extends React.Component{
     constructor(props){
         super(props)
@@ -14,6 +27,28 @@ export class SearchBar extends React.Component{
         this.setState({searchText:e.target.value})
         // thay vi log cai nay thi lam gi do de no filter ra nha!!!!
         console.log(this.state.searchText)
+    }
+    componentDidMount(){
+        // db.collection("users").add({
+        //     first: "Ada",
+        //     last: "Lovelace",
+        //     born: 1815
+        // })
+        // .then(function(docRef) {
+        //     console.log("Document written with ID: ", docRef.id);
+        // })
+        // .catch(function(error) {
+        //     console.error("Error adding document: ", error);
+        // });
+        let userRef = db.collection("users")
+        userRef
+        .where("job", "==", "jobless")
+        .get()
+        .then((querySnapshot) => {  // tìm trong api reference, query là truy vấn , mình phải chỉ định cái range = đặc tính 
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+            });
+        });
     }
     render(){
         return (
---
+++ src/config/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-WT11bPaqvYqQ42QenHHNNguDiwCI9eA",
+    authDomain: "dbp-js-d05.firebaseapp.com",
+    databaseURL: "https://dbp-js-d05.firebaseio.com",
+    projectId: "dbp-js-d05",
+    storageBucket: "dbp-js-d05.appspot.com",
+    messagingSenderId: "239630961808",
+    appId: "1:239630961808:web:70229f0faa2b4731875c15",
+    measurementId: "G-D11P6LJKH8"
+  };
