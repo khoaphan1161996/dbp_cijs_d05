ad07577701eaf26cd4193af9d901ec9deda6b104
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-17 20:46:34 +0700

    finish emoji tren lop

--- src/App.js
+++ src/App.js
@@ -5,25 +5,45 @@ import { Emoji } from './Components/Emoji'
 import React from 'react'
 import { emojiList } from './resources/emojiList'
 
-
-console.log(emojiList)
-function App() {
-  let elist_ = []
-  for (let i = 0; i < 20; i++) {
-    elist_.push(
-      <Emoji symbol={emojiList[i].symbol} title={emojiList[i].title} />
-    )
-  }
-
-
-  return (
-    <React.Fragment>
-      <Header />
-      <SearchBar />
-      { elist_}
-    </React.Fragment>
-
-  );
+function filterEmoji(searchText,data){
+  return data.filter((value)=>{
+    return value.keywords.indexOf(searchText)!=-1
+  })
 }
 
-export default App;
+// console.log(emojiList)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.createEmojis = this.createEmojis.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleInput(event){
+    // console.log(event.target.value)
+    this.setState({searchText:event.target.value})
+  }
+  createEmojis(emojis) {
+    let len = emojis.length > 20 ? 20 : emojis.length
+    let elist_ = []
+    for (let i = 0; i < len; i++) {
+      elist_.push(
+        <Emoji symbol={emojis[i].symbol} title={emojis[i].title} />
+      )
+    }
+    return elist_
+  }
+  render() {
+    let elist_ = this.createEmojis(filterEmoji(this.state.searchText,emojiList))
+    return (
+      <React.Fragment>
+        <Header />
+        <SearchBar onChange={this.handleInput} />
+        { elist_}
+      </React.Fragment>
+
+    );
+
+    }
+  }
+    export default App;
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -2,57 +2,56 @@ import '../assets/css/searchbar.css'
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import {firebaseConfig} from '../config/firebase.config'
+import { firebaseConfig } from '../config/firebase.config'
 import React from 'react'
 // di theo nhom
-try{
-    firebase.initializeApp(firebaseConfig);
-}catch(e){
-    console.log(e.message)
-}
+// try{
+//     firebase.initializeApp(firebaseConfig);
+// }catch(e){
+//     console.log(e.message)
+// }
 
-const db = firebase.firestore();
+// const db = firebase.firestore();
 
-export class SearchBar extends React.Component{
-    constructor(props){
-        super(props)
-        // this.setState =
-        this.state = {searchText:'hehe'}
+export function SearchBar(props) {
+    // constructor(props){
+    //     super(props)
+    //     // this.setState =
+    //     this.state = {searchText:'hehe'}
 
-        this.EnterInput = this.EnterInput.bind(this)
-    }
-    EnterInput(e){
-        // dung de handle su kien on change
-        // console.log("e.target.value")
-        this.setState({searchText:e.target.value})
-        // thay vi log cai nay thi lam gi do de no filter ra nha!!!!
-        console.log(this.state.searchText)
-    }
-    componentDidMount(){
-        // db.collection("users").add({
-        //     first: "Ada",
-        //     last: "Lovelace",
-        //     born: 1815
-        // })
-        // .then(function(docRef) {
-        //     console.log("Document written with ID: ", docRef.id);
-        // })
-        // .catch(function(error) {
-        //     console.error("Error adding document: ", error);
-        // });
-        let userRef = db.collection("users")
-        userRef
-        .where("job", "==", "jobless")
-        .get()
-        .then((querySnapshot) => {  // tìm trong api reference, query là truy vấn , mình phải chỉ định cái range = đặc tính 
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-            });
-        });
-    }
-    render(){
-        return (
-            <input onChange={this.EnterInput} className="full" type="text" placeholder="Enter your emoji..."/>
-        )
-    }
+    //     this.EnterInput = this.EnterInput.bind(this)
+    // }
+    // EnterInput(e){
+    //     // dung de handle su kien on change
+    //     // console.log("e.target.value")
+    //     this.setState({searchText:e.target.value})
+    //     // thay vi log cai nay thi lam gi do de no filter ra nha!!!!
+    //     // console.log(this.state.searchText)
+    // }
+    // componentDidMount(){
+    // db.collection("users").add({
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    // })
+    // .then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
+
+    // let userRef = db.collection("users")
+    // userRef
+    // .where("job", "==", "jobless")
+    // .get()
+    // .then((querySnapshot) => {  // tìm trong api reference, query là truy vấn , mình phải chỉ định cái range = đặc tính 
+    //     querySnapshot.forEach((doc) => {
+    //         console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+    //     });
+    // });
+    return (
+        <input onChange={props.handleInput} className="full" type="text" placeholder="Enter your emoji..." />
+    )
+
 }
