e4973df6d021cf5257ef7eed5d4df7f3f07384d6
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-20 23:11:12 +0700

    chatApp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { FriendContainer } from './Components/FriendContainer'
+import { MessagesContainer } from './Components/MessagesContainer'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container row">
+    {/* <> la react.fragment */}
+      <FriendContainer className="frcont child" />
+      <MessagesContainer className="mescont child" />
     </div>
-  );
+  )
 }
 
 export default App;
