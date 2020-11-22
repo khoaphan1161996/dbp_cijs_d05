baccaa53b2103f8e2dee54e28d0df8f87c7e0a0e
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-22 23:38:04 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -5,64 +5,65 @@ import { Header } from './components/Header'
 import { Button } from './components/Button'
 import { Show } from './components/Show'
 
-let array = []
-
-
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       meal: {
-        strMealThumb:"https://www.themealdb.com/images/media/meals/w8umt11583268117.jpg",
-        strMeal:"Madeira Cake",
-        strInstructions:"Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling.\r\nSift the flour and gently fold in, with enough milk to give a mixture that falls slowly from the spoon. Fold in the lemon zest.\r\nSpoon the mixture into the prepared tin and lightly level the top. Bake on the middle shelf of the oven for 30-40 minutes, or until golden-brown on top and a skewer inserted into the centre comes out clean.\r\nRemove from the oven and set aside to cool in the tin for 10 minutes, then turn it out on to a wire rack and leave to cool completely.\r\nTo serve, decorate the cake with the candied peel.",
+        strMealThumb: "https://www.themealdb.com/images/media/meals/w8umt11583268117.jpg",
+        strMeal: "Madeira Cake",
+        strInstructions: "Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling.\r\nSift the flour and gently fold in, with enough milk to give a mixture that falls slowly from the spoon. Fold in the lemon zest.\r\nSpoon the mixture into the prepared tin and lightly level the top. Bake on the middle shelf of the oven for 30-40 minutes, or until golden-brown on top and a skewer inserted into the centre comes out clean.\r\nRemove from the oven and set aside to cool in the tin for 10 minutes, then turn it out on to a wire rack and leave to cool completely.\r\nTo serve, decorate the cake with the candied peel.",
         strCategory: "Dessert",
         strArea: "British",
         strTags: "Cake,Light,Baking,Desert",
-        strIngredient: "Butter",
+        strIngredient: "",
         strYoutube: "https://www.youtube.com/watch?v=-YDh4WEmK_E",
-    }
-    }
-    this.getMeal = this.getMeal.bind(this)
-    this.Ingre = this.Ingre.bind(this)
-  }
-  getMeal(){
-    const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      return this.setState({meal:{
-        strMealThumb:`${data.meals[0].strMealThumb}`,
-        strMeal:data.meals[0].strMeal,
-        strInstructions:data.meals[0].strInstructions,
-        strCategory:data.meals[0].strCategory,
-        strArea:data.meals[0].strArea,
-        strTags:data.meals[0].strTags,
-        strIngredient:array,
-        strYoutube:data.meals[0].strYoutube,
-    }})
-    })
-  }
-  Ingre(){
-    for(let i = 0 ; i < 20 ; i ++){
-      if(data.meals[0][`strIngredient${i}`] != ""){
-        array.push()
       }
     }
+    this.getMeal = this.getMeal.bind(this)
+  }
+  getMeal() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        return this.setState({
+          meal: {
+            strMealThumb: `${data.meals[0].strMealThumb}`,
+            strMeal: data.meals[0].strMeal,
+            strInstructions: data.meals[0].strInstructions,
+            strCategory: data.meals[0].strCategory,
+            strArea: data.meals[0].strArea,
+            strTags: data.meals[0].strTags,
+            strIngredient: this.Ingre(data) ,
+            strYoutube: data.meals[0].strYoutube,
+          }
+        })
+      })
+  }
+  Ingre(data){
+    let array = []
+    for(let i = 1 ; i <= 20 ; i++){
+      if(data.meals[0][`strIngredient${[i]}`] != ""){
+      array.push(<li>{data.meals[0][`strIngredient${[i]}`]} - {data.meals[0][`strMeasure${[i]}`]}</li>
+      )
+  }
+  }
+    return array
   }
   render() {
     return (
       <div className="App">
         <Header></Header>
         <Button click={this.getMeal}></Button>
-        <Show strMealThumb={this.state.meal.strMealThumb} 
-        strMeal={this.state.meal.strMeal} 
-        strInstructions={this.state.meal.strInstructions}
-        strCategory={this.state.meal.strCategory}
-        strArea={this.state.meal.strArea}
-        strTags={this.state.meal.strTags}
-        strIngredient={this.state.meal.strIngredient}
-        strYoutube={this.state.meal.strYoutube}
+        <Show strMealThumb={this.state.meal.strMealThumb}
+          strMeal={this.state.meal.strMeal}
+          strInstructions={this.state.meal.strInstructions}
+          strCategory={this.state.meal.strCategory}
+          strArea={this.state.meal.strArea}
+          strTags={this.state.meal.strTags}
+          strIngredient={this.state.meal.strIngredient}
+          strYoutube={this.state.meal.strYoutube}
         ></Show>
       </div>
     );
--- src/components/Show.js
+++ src/components/Show.js
@@ -4,22 +4,25 @@ export function Show(props) {
     return (
         <div>
             <div className="row">
-                <img className="ava" src={props.strMealThumb}></img>
+                <div>
+                    <img className="ava" src={props.strMealThumb}></img>
+                    <p class="CAT"><strong>Category: </strong>{props.strCategory}</p>
+                    <p class="CAT"><strong>Area: </strong>{props.strArea}</p>
+                    <p class="CAT"><strong>Tags: </strong>{props.strTags}</p>
+                    <h2 class="title2">Ingredients:</h2>
+                    <ul class="CAT">
+                        {props.strIngredient}
+                    </ul>
+                    <h2 class="title2">Video Recipe:</h2>
+                    <iframe width="560" height="315" className="yt" src={props.strYoutube.replace('watch?v=', 'embed/')} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                
                 <div>
                     <h4 className="name">{props.strMeal}</h4>
                     <p className="instrustion">{props.strInstructions}</p>
                 </div>
             </div>
-            <div>
-                <p class="CAT"><strong>Category: </strong>{props.strCategory}</p>
-                <p class="CAT"><strong>Area: </strong>{props.strArea}</p>
-                <p class="CAT"><strong>Tags: </strong>{props.strTags}</p>
-                <h2 class="CAT">Ingredients:</h2>
-                <ul>
-                    <li>{props.strIngredient}</li>
-                </ul>
-                <iframe width="560" height="315" className="yt" src={props.strYoutube.replace('watch?v=','embed/')} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
+
         </div>
     )
 }
