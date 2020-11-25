dd056b3338ab21ec0793aed5d9441b42d735bcb5
khoaphan1161996 <khoa071996@gmail.com>
2020-11-25 20:30:16 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header'
-import { Search } from './components/Search'
-import { Show } from './components/Show'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      books : [],
-      find : ''
-    }
-    this.Findbook = this.Findbook.bind(this)
-    this.onChange = this.onChange.bind(this)
-  }
-  Findbook(){
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.find}`)
-    .then(res=>res.json())
-    .then(data => {console.log(data)
-      let books = []
-    let n = data.length
-    for(let i = 0 ; i < n ; i ++ ){
-      books.push({
-          <Show/>
-      })  
-    }
-    })
-  }
-  onChange(e){
-    this.setState({
-      find: e.target.value
-    })
-    // console.log(this.state.find)
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Search onChange={this.onChange} onClick={this.Findbook} />
-        <div className="books">
-
-        </div>
-
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import '../assets/css/Header.css'
-
-export function Header(props){
-    return (
-        <div className="Header">
-            <h1>Hello, this is Book-Finder-App</h1>
-        </div>
-    )
-}
