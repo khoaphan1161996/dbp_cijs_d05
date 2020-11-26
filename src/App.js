c541b2f6d05f401b9061f7360830014ee14a9511
khoa071996@gmail.com <khoa071996@gmail.com>
2020-11-26 19:29:39 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ class App extends React.Component {
     super(props)
     this.state = {
       meal: {
-        strMealThumb: "https://www.themealdb.com/images/media/meals/w8umt11583268117.jpg",
-        strMeal: "Madeira Cake",
-        strInstructions: "Pre-heat the oven to 180C/350F/Gas 4. Grease an 18cm/7in round cake tin, line the base with greaseproof paper and grease the paper.\r\nCream the butter and sugar together in a bowl until pale and fluffy. Beat in the eggs, one at a time, beating the mixture well between each one and adding a tablespoon of the flour with the last egg to prevent the mixture curdling.\r\nSift the flour and gently fold in, with enough milk to give a mixture that falls slowly from the spoon. Fold in the lemon zest.\r\nSpoon the mixture into the prepared tin and lightly level the top. Bake on the middle shelf of the oven for 30-40 minutes, or until golden-brown on top and a skewer inserted into the centre comes out clean.\r\nRemove from the oven and set aside to cool in the tin for 10 minutes, then turn it out on to a wire rack and leave to cool completely.\r\nTo serve, decorate the cake with the candied peel.",
-        strCategory: "Dessert",
-        strArea: "British",
-        strTags: "Cake,Light,Baking,Desert",
+        strMealThumb: "",
+        strMeal: "",
+        strInstructions: "",
+        strCategory: "",
+        strArea: "",
+        strTags: "",
         strIngredient: "",
-        strYoutube: "https://www.youtube.com/watch?v=-YDh4WEmK_E",
+        strYoutube: "",
       }
     }
     this.getMeal = this.getMeal.bind(this)
@@ -52,21 +52,32 @@ class App extends React.Component {
     return array
   }
   render() {
-    return (
-      <div className="App">
-        <Header></Header>
-        <Button click={this.getMeal}></Button>
-        <Show strMealThumb={this.state.meal.strMealThumb}
-          strMeal={this.state.meal.strMeal}
-          strInstructions={this.state.meal.strInstructions}
-          strCategory={this.state.meal.strCategory}
-          strArea={this.state.meal.strArea}
-          strTags={this.state.meal.strTags}
-          strIngredient={this.state.meal.strIngredient}
-          strYoutube={this.state.meal.strYoutube}
-        ></Show>
-      </div>
-    );
+    if(this.state.meal.strMealThumb != ""){
+      return (
+        <div className="App">
+          <Header></Header>
+          <Button click={this.getMeal}></Button>
+          <Show strMealThumb={this.state.meal.strMealThumb}
+            strMeal={this.state.meal.strMeal}
+            strInstructions={this.state.meal.strInstructions}
+            strCategory={this.state.meal.strCategory}
+            strArea={this.state.meal.strArea}
+            strTags={this.state.meal.strTags}
+            strIngredient={this.state.meal.strIngredient}
+            strYoutube={this.state.meal.strYoutube}
+          ></Show>
+        </div>
+      );
+    }
+    
+    else {
+      return (
+        <div className="App">
+          <Header></Header>
+          <Button click={this.getMeal}></Button>
+        </div>
+      );
+    }
   }
 }
 
